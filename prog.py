95e9e02944a334cdc770b15549cb94d3e49163a6
Gero-B-Anonuevo <150780703+Gero-B-Anonuevo@users.noreply.github.com>
2024-04-29 11:49:27 +0800

    Update prog.py

---
+++ prog.py
@@ -0,0 +1,5 @@
+full_name = input("Please input your full name: ")
+print(f"Full Name: {full_name}.")
+print(f"First Character: {full_name[0]}.")
+print(f"First Five Characters: {", ".join(full_name[0:5])}.")
+print(f"Fourth, Fifth, and Sixth Characters: {", ".join(full_name[3:6])}.")
